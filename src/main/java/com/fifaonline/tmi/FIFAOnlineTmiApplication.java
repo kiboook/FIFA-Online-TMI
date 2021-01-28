cd5fe801f77e3f9ef0c7774da74006ecb05d4d26
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-28 22:22:53 +0900

    [2021.01.28] First Commit

---
+++ src/main/java/com/fifaonline/tmi/FIFAOnlineTmiApplication.java
@@ -0,0 +1,12 @@
+package com.fifaonline.tmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIFAOnlineTmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FIFAOnlineTmiApplication.class, args);
+    }
+}
