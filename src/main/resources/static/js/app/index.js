05227bb23583ed5df6c4c600a7daec0585bd9e92
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-01 23:37:12 +0900

    [2021.02.01] 구단주 예외 처리 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,19 +5,22 @@ var main = {
             _this.search_user_info();
         });
     },
-    search_user_info : function () {
+    search_user_info: function () {
         const nickname = $('#nickname').val();
-
-        $.ajax({
-            type: 'GET',
-            url: '/api/v1/user/'+nickname,
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function (res) {
-            alert(JSON.stringify(res));
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+        if (nickname === '') {
+            alert("구단주를 입력하세요!");
+        } else {
+            $.ajax({
+                type: 'GET',
+                url: '/api/v1/user/' + nickname,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8'
+            }).done(function (res) {
+                alert(JSON.stringify(res));
+            }).fail(function (error) {
+                alert("존재하지 않는 구단주입니다!");
+            });
+        }
     }
 };
 
