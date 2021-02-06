8f89a958f906944cd365ff2fbfadfeb790c162c7
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-07 00:56:34 +0900

    [2021.02.07] 유저 역대 최고 등급 조회 구현 중

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,13 +1,14 @@
-const main = {
+var main = {
     init: function () {
         const _this = this;
+
+        // 유저 닉네임 검색 버튼 이벤트
         const searchBtn = document.querySelector("#btn-user-info");
         if (searchBtn) {
-            searchBtn.addEventListener('click', function () {
-                _this.search_user_info();
-            });
+            searchBtn.addEventListener('click', _this.search_user_info);
         }
 
+        // 유저 닉네임 검색 Enter 키 이벤트
         const searchEnter = document.querySelector("#input-nickname");
         if (searchEnter) {
             searchEnter.addEventListener('keyup', key => {
@@ -18,7 +19,7 @@ const main = {
         }
     },
     search_user_info: function () {
-        const nickname = document.querySelector("#nickname").value;
+        const nickname = document.querySelector("#input-nickname").value;
 
         if (nickname === '') {
             alert("구단주를 입력하세요!");
@@ -26,9 +27,10 @@ const main = {
             $.ajax({
                 type: 'GET',
                 url: `/api/v1/user/${nickname}`
-            }).done(function (nickname) {
+            }).done(nickname => {
                 window.location.href = `/user/info/${nickname}`;
-            }).fail(function () {
+            }).fail(err => {
+                console.log(err);
                 alert("존재하지 않는 구단주입니다!");
             });
         }
