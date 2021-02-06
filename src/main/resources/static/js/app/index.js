98f54ef456a1b85e72244e0e04deb23a04201649
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-06 19:27:25 +0900

    [2021.02.06] JQuery -> 바닐라 JS (ajax 제외)

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,26 +1,33 @@
-var main = {
+const main = {
     init: function () {
-        var _this = this;
-        $('#btn-user-info').on('click', function () {
-            _this.search_user_info();
-        });
-
-        $('#nickname').on('keyup', function (key) {
-            if (key.keyCode === 13) {
+        const _this = this;
+        const searchBtn = document.querySelector("#btn-user-info");
+        if (searchBtn) {
+            searchBtn.addEventListener('click', function () {
                 _this.search_user_info();
-            }
-        })
+            });
+        }
+
+        const searchEnter = document.querySelector("#nickname");
+        if (searchEnter) {
+            searchEnter.addEventListener('keyup', key => {
+                if (key.keyCode === 13) {
+                    _this.search_user_info();
+                }
+            })
+        }
     },
     search_user_info: function () {
-        const nickname = $('#nickname').val();
+        const nickname = document.querySelector("#nickname").value;
+
         if (nickname === '') {
             alert("구단주를 입력하세요!");
         } else {
             $.ajax({
                 type: 'GET',
-                url: '/api/v1/user/' + nickname,
+                url: `/api/v1/user/${nickname}`
             }).done(function (nickname) {
-                window.location.href = '/user/info/' + nickname;
+                window.location.href = `/user/info/${nickname}`;
             }).fail(function () {
                 alert("존재하지 않는 구단주입니다!");
             });
