3ee6fef3ae1033f8b305c417623aa833d4d3e64a
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-07 18:19:05 +0900

    [2021.02.07] UserResponseDto -> UserInfoResponseDto

--- src/main/java/com/fifaonline/tmi/web/IndexController.java
+++ src/main/java/com/fifaonline/tmi/web/IndexController.java
@@ -1,7 +1,7 @@
 package com.fifaonline.tmi.web;
 
 import com.fifaonline.tmi.service.UserService;
-import com.fifaonline.tmi.web.dto.UserResponseDto;
+import com.fifaonline.tmi.web.dto.UserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,14 +20,13 @@ public class IndexController {
 
     @GetMapping("/user/info/{nickname}")
     public String userInfo(@PathVariable String nickname, Model model) {
-        UserResponseDto userResponseDto = userService.findById(nickname);
+        UserInfoResponseDto userResponseDto = userService.findById(nickname);
         model.addAttribute("user", userResponseDto);
         return "user-info";
     }
 
     @GetMapping("/user/division/{nickname}")
     public String userDivision(@PathVariable String nickname) {
-        // nickname 가지고 DB 에서 유저 조회 한 뒤 accessId 통해 역대 최고 등급 API 찌르기
         return "user-division";
     }
 }
