d17cdba31f1efb85ce544126d1bb733ea31178ce
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-07 23:42:53 +0900

    [2021.02.07] 유저 최고 등급 검색 구현

--- src/main/java/com/fifaonline/tmi/web/UserApiController.java
+++ src/main/java/com/fifaonline/tmi/web/UserApiController.java
@@ -2,6 +2,7 @@ package com.fifaonline.tmi.web;
 
 import com.fifaonline.tmi.service.UserService;
 import com.fifaonline.tmi.web.dto.UserApiResponseDto;
+import com.fifaonline.tmi.web.dto.UserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,5 +17,11 @@ public class UserApiController {
     public String requestUserInfo(@PathVariable String nickname) {
         UserApiResponseDto userApiResponseDto = userService.requestUserInfo(nickname);
         return userService.save(userApiResponseDto);
+    }
+
+    @GetMapping("/api/v1/user/division/{nickname}")
+    public String requestUserAccessID(@PathVariable String nickname) {
+        UserInfoResponseDto userInfoResponseDto = userService.findById(nickname);
+        return userInfoResponseDto.getAccessId();
     }
 }
