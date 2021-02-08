7271d16c13927ecb632496e4bcb302f43638cc60
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-08 19:15:55 +0900

    [2021.02.08] API KEY 반환 API 생성

--- src/main/java/com/fifaonline/tmi/web/UserApiController.java
+++ src/main/java/com/fifaonline/tmi/web/UserApiController.java
@@ -1,5 +1,6 @@
 package com.fifaonline.tmi.web;
 
+import com.fifaonline.tmi.config.ApiKey;
 import com.fifaonline.tmi.service.UserService;
 import com.fifaonline.tmi.web.dto.UserApiResponseDto;
 import com.fifaonline.tmi.web.dto.UserInfoResponseDto;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
+
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
+    @Inject
+    private ApiKey apiKey;
     private final UserService userService;
 
     @GetMapping("/api/v1/user/{nickname}")
@@ -23,5 +28,10 @@ public class UserApiController {
     public String requestUserAccessID(@PathVariable String nickname) {
         UserInfoResponseDto userInfoResponseDto = userService.findById(nickname);
         return userInfoResponseDto.getAccessId();
+    }
+
+    @GetMapping("/api/v1/key")
+    public String requestApiKey() {
+        return apiKey.getKey();
     }
 }
