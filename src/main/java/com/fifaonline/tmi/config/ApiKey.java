91378eeedb11db94ac08c9179c7638720c41a5fe
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-08 19:14:46 +0900

    [2021.02.08] API KEY 반환 객체 싱글톤 적용

--- src/main/java/com/fifaonline/tmi/config/ApiKey.java
+++ src/main/java/com/fifaonline/tmi/config/ApiKey.java
@@ -10,4 +10,16 @@ import org.springframework.stereotype.Component;
 public class ApiKey {
     @Value("${api-key}")
     private String key;
+
+    private static ApiKey instance = new ApiKey();
+
+    private ApiKey() {
+    }
+
+    public static ApiKey getInstance() {
+        if (instance == null) {
+            instance = new ApiKey();
+        }
+        return instance;
+    }
 }
